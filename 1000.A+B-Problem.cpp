bd33f84b8ac43005e180b6d9b0047041a1550817
Berlin <evan1234.ek@gmail.com>
2022-12-13 17:05:46 +0600

    No. 1000 problem uploaded

---
+++ 1000.A+B-Problem.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+
+    // Code Start From Here
+
+    int num1, num2;
+
+    cin >> num1 >> num2;
+
+    int sum = num1 + num2;
+
+    cout << sum << endl;
+
+    return 0;
+}
