29f030b1fded450b6c19590ead8d1e7eb5668552
Berlin <evan1234.ek@gmail.com>
2022-12-13 17:11:59 +0600

    1264 no. problem added

---
+++ 1264.Workdays.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+
+    // Code Start From Here
+
+    int N, M;
+
+    cin >> N >> M;
+
+    int sum = N * (M + 1);
+
+    cout << sum << endl;
+
+    return 0;
+}
