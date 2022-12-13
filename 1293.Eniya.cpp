35736664c60107eb89e6fc9885429791bff3172a
Berlin <evan1234.ek@gmail.com>
2022-12-13 17:14:28 +0600

    Added

---
+++ 1293.Eniya.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+
+    // Code Start From Here
+
+    int N, a, b;
+
+    cin >> N >> a >> b;
+
+    int sum = 2 * N * a * b;
+
+    cout << sum << endl;
+
+    return 0;
+}
