3395292f31d0fa43193ae39ce5687e03d760bc24
Berlin <evan1234.ek@gmail.com>
2022-12-13 17:21:54 +0600

    Added

---
+++ 1409.TwoGangsters.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+
+    // Code Start From Here
+
+    int h, l;
+
+    cin >> h >> l;
+
+    int nshotBHarry = l - 1;
+
+    int nshotBLarry = h - 1;
+
+    cout << nshotBHarry << " " << nshotBLarry;
+
+    return 0;
+}
